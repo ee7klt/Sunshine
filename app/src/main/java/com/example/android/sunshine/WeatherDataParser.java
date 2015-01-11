e3823bbf0599c5f01061e0cd66ba19e6ee0916f2
ee7klt <kltah1@gmail.com>
2015-01-11 11:37:08 +0800

    parse out max temp

---
+++ app/src/main/java/com/example/android/sunshine/WeatherDataParser.java
@@ -0,0 +1,27 @@
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WeatherDataParser {
+
+    /**
+     * Given a string of the form returned by the api call:
+     * http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7
+     * retrieve the maximum temperature for the day indicated by dayIndex
+     * (Note: 0-indexed, so 0 would refer to the first day).
+     */
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+            throws JSONException {
+        // TODO: add parsing code here
+        JSONObject parseWeatherObj = new JSONObject(weatherJsonStr);
+        JSONArray weathers = parseWeatherObj.getJSONArray("list");
+        JSONObject temp = weathers.getJSONObject("temp");
+        JSONObject max = temp.getJSONObject("max");
+        Log.d("weathers",weathers.toString());
+
+        return Double.parseDouble(max.toString());
+    }
+
+}
