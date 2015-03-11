0dc511beb62486d96978804ec82db74159006ad9
ee7klt <kltah1@gmail.com>
2015-03-11 12:36:01 +0800

    forecastItem in toast

--- app/src/main/java/com/example/android/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/ForecastFragment.java
@@ -99,9 +99,9 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Context context = getActivity();
-                CharSequence text = "Hello toast!";
+                String forecastItem = forecastAdapter.getItem(position);
                 int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(context, text,duration);
+                Toast toast = Toast.makeText(context, forecastItem,duration);
                 toast.show();
             }
         });
